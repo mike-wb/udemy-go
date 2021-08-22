79dc8be5248cd01aa7acd8abbc8ae53dc41e7520
Michael WB <michael@wb.media>
2021-08-22 17:19:03 -0400

    Changed router to chi and added simple middleware

--- hello-web/cmd/web/routes.go
+++ hello-web/cmd/web/routes.go
@@ -3,16 +3,21 @@ package main
 import (
 	"net/http"
 
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 	"github.com/mike-wb/udemy-go/pkg/config"
 	"github.com/mike-wb/udemy-go/pkg/handlers"
 )
 
+// routes sets up the routes for this application
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
+	mux := chi.NewRouter()
 
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	mux.Use(middleware.Recoverer)
+	mux.Use(WriteToConsole)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 
 	return mux
 }
