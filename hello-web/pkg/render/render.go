3807384ee9ccace59ab9fdc230950f8edc184458
Michael WB <michael@wb.media>
2021-08-22 16:07:57 -0400

    Added layout support

--- hello-web/pkg/render/render.go
+++ hello-web/pkg/render/render.go
@@ -1,20 +1,70 @@
 package render
 
 import (
+	"bytes"
 	"fmt"
+	"log"
 	"net/http"
+	"path/filepath"
 	"text/template"
 )
 
+var functions = template.FuncMap{}
+
+// RenderTemplates renders templates using html/template
 func RenderTemplates(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, err := template.ParseFiles("./templates/" + tmpl)
+	tc, err := CreateTemplateCache()
 	if err != nil {
-		fmt.Fprint(w, "Error loading template: ", tmpl)
+		log.Fatal(err)
+	}
+
+	t, ok := tc[tmpl]
+	if !ok {
+		log.Fatal(fmt.Sprintf("template not found: %s", tmpl))
+	}
+
+	buf := new(bytes.Buffer)
+	_ = t.Execute(buf, nil)
+	_, err = buf.WriteTo(w)
+
+	//parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	//err = parsedTemplate.Execute(w, nil)
+	if err != nil {
+		fmt.Fprint(w, "error parsing template: ", err)
 		return
 	}
-	parseError := parsedTemplate.Execute(w, nil)
-	if parseError != nil {
-		fmt.Fprint(w, "Error parsing template: ", tmpl)
-		return
+}
+
+// CreateTemplateCache creates a template cache as a map
+func CreateTemplateCache() (map[string]*template.Template, error) {
+	myCache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob("./templates/*.page.tmpl")
+	if err != nil {
+		return myCache, err
 	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
+		if err != nil {
+			return myCache, err
+		}
+
+		matches, err := filepath.Glob("./templates/*.layout.tmpl")
+		if err != nil {
+			return myCache, err
+		}
+
+		if len(matches) > 0 {
+			ts, err = ts.ParseGlob("./templates/*.layout.tmpl")
+			if err != nil {
+				return myCache, err
+			}
+
+		}
+
+		myCache[name] = ts
+	}
+	return myCache, nil
 }
